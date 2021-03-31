6350deb97fc00814df01222a2309049e26609e13
Jae No <jaeyno@gmail.com>
2021-03-30 20:14:37 -0400

    Added generic inteface & repository -first method to get a item -second method to get all items

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
