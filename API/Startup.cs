4244af1a3b284507a7f5ceae1d06d7b2344ffec3
Jae No <jaeyno@gmail.com>
2021-04-02 15:27:17 -0400

    Added CORS support to API

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,13 @@ namespace API
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +58,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
