6b2bd34f4ac0c93b8df7761a8b42d428c1361f7e
Jae No <jaeyno@gmail.com>
2021-04-03 22:15:57 -0400

    Added filter feature to the clident side

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,3 +1,5 @@
+import { IType } from './../models/productType';
+import { IBrand } from './../models/brand';
 import { ShopService } from './shop.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../models/product';
@@ -10,15 +12,51 @@ import { IProduct } from '../models/product';
 export class ShopComponent implements OnInit {
 
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected: number = 0;
+  typeIdSelected: number = 0;
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProduct().subscribe(response => {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProduct(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log(error);
     })
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = [{id: 0, name: 'All'}, ...response]
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
   }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,9 @@
+import { IBrand } from './../models/brand';
 import { IPagination } from './../models/pagination';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IType } from '../models/productType';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,31 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProduct() {
-    return this.http.get<IPagination>(this.baseUrl + 'products');
+  getProduct(brandId?: number, typeId?: number) {
+    
+    let params = new HttpParams();
+
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
