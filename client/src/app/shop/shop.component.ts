1f464f06a1675301f8161654756db210357a5f10
Jae No <jaeyno@gmail.com>
2021-04-04 23:09:44 -0400

    Added search feature on client side

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -2,7 +2,7 @@ import { ShopParams } from './../models/shopParams';
 import { IType } from './../models/productType';
 import { IBrand } from './../models/brand';
 import { ShopService } from './shop.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IProduct } from '../models/product';
 
 @Component({
@@ -12,6 +12,7 @@ import { IProduct } from '../models/product';
 })
 export class ShopComponent implements OnInit {
 
+  @ViewChild('search', {static: true}) searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -75,6 +76,17 @@ export class ShopComponent implements OnInit {
 
   onPageChanged(event: any) {
     this.shopParams.pageNumber = event;
+    this.getProducts();
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
     this.getProducts();
   }
 
