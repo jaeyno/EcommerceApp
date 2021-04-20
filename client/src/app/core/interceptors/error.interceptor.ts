d6994d18b05c963655978352aa094854a1ed4260
Jae No <jaeyno@gmail.com>
2021-04-19 21:28:53 -0400

    Using toast to show the error message

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -3,16 +3,27 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError(error => {
         if (error) {
+          if (error.status === 400) {
+            if (error.error.errors) {
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+          if (error.status === 401) {
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
           if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
