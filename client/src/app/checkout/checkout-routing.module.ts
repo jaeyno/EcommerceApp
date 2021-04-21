27029da1f9da560a1f7f9cbdad3991801ce13e75
Jae No <jaeyno@gmail.com>
2021-04-20 23:58:55 -0400

    Added checkout module and component

---
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -0,0 +1,19 @@
+import { CheckoutComponent } from './checkout.component';
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: CheckoutComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class CheckoutRoutingModule { }
