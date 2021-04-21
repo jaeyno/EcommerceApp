4374af1bcfa978d20d5d05f256565c0338c72355
Jae No <jaeyno@gmail.com>
2021-04-20 23:45:14 -0400

    Added the increment & decrement functionality to basket component

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,5 +1,5 @@
 import { BasketService } from './basket.service';
-import { IBasket } from './../models/basket';
+import { IBasket, IBasketItem } from './../models/basket';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -18,4 +18,15 @@ export class BasketComponent implements OnInit {
     this.basket$ = this.basketService.basket$;
   }
 
+  removeBasketItem(item: IBasketItem) {
+    this.basketService.removeItemFromBasket(item);
+  }
+
+  incrementItemQuantity(item: IBasketItem) {
+    this.basketService.incrementItemQuantity(item);
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    this.basketService.decrementItemQuantity(item);
+  }
 }
