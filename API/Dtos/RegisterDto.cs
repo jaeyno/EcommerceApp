5220de6eed92a7cd13bc139696245298c15bfcd6
Jae No <jaeyno@gmail.com>
2021-04-21 20:50:40 -0400

    Added validation on server side

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,20}$", ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non-alphabetical character")]
         public string Password { get; set; }
     }
 }
